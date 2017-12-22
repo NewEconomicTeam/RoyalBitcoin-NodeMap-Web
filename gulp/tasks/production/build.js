b51b1ece73ced6430e2d3798b86abf0b081bc86a
Moore.Huang <moore.hy@gmail.com>
2017-12-22 17:13:39 +0800

    F: 实现生产用Build任务

---
+++ gulp/tasks/production/build.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var runSequence = require('run-sequence');
+
+/**
+ * 按定义顺序执行全部构建任务
+ */
+gulp.task('build:prod', ['build'], function(callback) {
+  runSequence(
+    'delete:prod',
+    [
+      'html:prod',
+      'optimize:html',
+      'optimize:css',
+      // 'optimize:js',
+      // 'optimize:images'
+    ],
+    // 'revision',
+    // 'rev:collect',
+    callback);
+});
