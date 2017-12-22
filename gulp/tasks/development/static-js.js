108cd6ceb783aa8ecafd4e14122f4341cfea00b9
Moore.Huang <moore.hy@gmail.com>
2017-12-22 18:13:03 +0800

    O: 优化js静态文件拷贝任务

--- gulp/tasks/development/static-js.js
+++ gulp/tasks/development/static-js.js
@@ -1,16 +1,16 @@
 var gulp = require('gulp');
 var changed = require('gulp-changed');
-var browserSync = require('browser-sync');
+var browsersync = require('browser-sync');
 var config = require('../../config').staticJS;
 
-/*
-  拷贝变化的图片到构建目录下
-*/
+/**
+ * 拷贝变化的静态文件(相关库)到构建目录下
+ */
 gulp.task('static-js', function() {
-  browserSync.notify('拷贝静态文件');
+  browsersync.notify('拷贝静态文件');
 
   return gulp.src(config.src)
     .pipe(changed(config.dest))  // 忽略未变化的文件
     .pipe(gulp.dest(config.dest))
-    .pipe(browserSync.stream());
+    .pipe(browsersync.stream());
 });
